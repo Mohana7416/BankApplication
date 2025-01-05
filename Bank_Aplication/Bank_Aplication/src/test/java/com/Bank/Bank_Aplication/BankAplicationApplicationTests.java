877fcee6e1e2379791c12c9b42314870b387aa9b
MohanaKrishna <mohana741656@gmail.com>
2025-01-05 23:05:52 +0530

    Fist Message

---
+++ Bank_Aplication/Bank_Aplication/src/test/java/com/Bank/Bank_Aplication/BankAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bank.Bank_Aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
